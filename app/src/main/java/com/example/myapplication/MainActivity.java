2e39a8ac933f11e6712f14c1ebd163d53b2c3109
jaemin <o0xvx0o5@naver.com>
2020-07-05 21:49:15 +0900

    second

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,14 +7,15 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        int a=13;
     }
     public void onButton1(View v){
         Toast.makeText(this,"확인1버튼이눌렸다",Toast.LENGTH_LONG).show();// 작고 간단한 메시지를 잠깐 보여주는 역할을 한다.
